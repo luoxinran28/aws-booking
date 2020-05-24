650330f785f678be75523f0d836e7961e2e0279e
luoxinran28 <luoxinran28@gmail.com>
2020-05-23 20:56:46 -0600

    Only show the limited items in one page

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -24,20 +24,29 @@ class Products extends Component {
     this.setState({ currentPage });
   }
 
+  getPagedData = () => {
+    const { products: allProducts, pageSize, currentPage } = this.state;
+    const startIndex = (currentPage - 1) * pageSize;
+    let endIndex = (currentPage) * pageSize - 1;
+    endIndex = endIndex >= allProducts.length ? allProducts.length - 1 : endIndex;
+    const currentPagedData = allProducts.slice(startIndex, endIndex + 1);
+    return { totalCount: allProducts.length, currentPageData: currentPagedData};
+  }
+
   componentDidMount() { 
     const products = getProducts();
     this.setState({ products });
   }
 
   render() { 
-    const { products, pageSize, currentPage } = this.state;
-    const totalCount = products.length;
+    const { pageSize, currentPage } = this.state;
+    const { totalCount, currentPageData: products } = this.getPagedData();
 
     if (totalCount === 0) return <h1>The products ran out.</h1>
 
     return (
       <React.Fragment>
-        <p>We found {products.length} for you.</p>
+        <p>We found {totalCount} products for you.</p>
         <button
           onClick={this.handleReset}
           className="btn btn-primary">Reset</button>
